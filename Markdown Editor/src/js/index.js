bcb82d506b0dcdc3f3e8896c16adc13b8c12f5d6
Rendxx <dren@ugridd.com>
2015-10-05 13:47:28 -0500

    v1.0.0 is FInish

---
+++ Markdown Editor/src/js/index.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+    var mdInput = document.getElementById('mdInput');
+    var mdPreview = document.getElementById('mdPreview');
+    var $mdInput = $(mdInput);
+    var $mdInputWrap = $('.md-input');
+
+    mdInput.addEventListener('keyup', function (e) {
+        mdPreview.innerHTML = marked(mdInput.value);
+    });
+    
+    var w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0];
+
+    $(window).resize(function () {
+        var h = (w.innerHeight || e.clientHeight || g.clientHeight) - 120;
+        $mdInputWrap.height(h);
+        $mdInput.height(h - 42);
+    });
+    $(window).resize();
+});
